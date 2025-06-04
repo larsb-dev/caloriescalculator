8c86d5f870a568a221399c4ab0fb934827f25e2c
larsb-dev <github@worldagainst.me>
2025-06-04 14:38:43 +0200

    add Person class

---
+++ src/main/Person.java
@@ -0,0 +1,65 @@
+package main;
+
+import java.time.Year;
+
+public class Person {
+    private String name = "";
+    private Sex sex = Sex.MALE;
+    private int weight = 0;
+    private int height = 0;
+    private int birthYear = 0;
+    private int age = 0;
+    private double dailyCalories = 0;
+    private ActivityLevel activityLevel = ActivityLevel.MODERATELY_ACTIVE;
+
+    public Person(String name, Sex sex, int weight, int height, int birthYear) {
+        this.name = name;
+        this.sex = sex;
+        this.weight = weight;
+        this.height = height;
+        this.birthYear = birthYear;
+
+        calcAge(birthYear);
+    }
+
+    private void calcAge(int birthYear) {
+        int currentYear = Year.now().getValue();
+        age = currentYear - birthYear;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Sex getSex() {
+        return sex;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public ActivityLevel getActivityLevel() {
+        return activityLevel;
+    }
+
+    public double getDailyCalories() {
+        return dailyCalories;
+    }
+
+    public void setDailyCalories(double dailyCalories) {
+        this.dailyCalories = dailyCalories;
+    }
+
+    public void setActivityLevel(ActivityLevel activityLevel) {
+        this.activityLevel = activityLevel;
+    }
+}
